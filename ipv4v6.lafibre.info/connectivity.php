8a9de2089c5384705d9ce3d0581f282461777c51
William Desportes <williamdes@wdes.fr>
2022-10-28 15:28:15 +0200

    Refresh connectivity.php

--- ipv4v6.lafibre.info/connectivity.php
+++ ipv4v6.lafibre.info/connectivity.php
@@ -1,13 +1,14 @@
 <?php
-$hostname = htmlspecialchars(gethostbyaddr($_SERVER['REMOTE_ADDR']));
-echo '{
-    "literal-connectivity": "<span class=textevert>OK</span>",
-    "literal-ip": " : IPv4 publique = ',$_SERVER['REMOTE_ADDR'],'",
-    "literal-remote_port": " et ';
-if ($_SERVER['REMOTE_PORT'] < "32768")
-	echo '<span class=texterouge>TCP ';
-else
-	echo '<span class=textevert>TCP ';
-echo $_SERVER['REMOTE_PORT'],'</span> (via IPv4 littérale)"
-}';
-?>
+header('Content-Type: application/json');
+
+// See: https://lafibre.info/tcpip/local_port_range/
+$isEphemeralPort = ((int) $_SERVER['REMOTE_PORT']) < 32768;
+
+echo json_encode([
+    'literal-connectivity' => '<span class="textevert">OK</span>',
+    'literal-ip' => ' : IPv4 publique = ' . $_SERVER['REMOTE_ADDR'],
+    'literal-remote_port' => ' et <span class="'
+        . ($isEphemeralPort ? 'texterouge' : 'textevert') . '">TCP '
+        . ($isEphemeralPort ? '' : $_SERVER['REMOTE_PORT'])
+        . '</span> (via IPv4 littérale)',
+], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
